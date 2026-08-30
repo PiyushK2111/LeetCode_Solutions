ab702a1e8ff8e5e901d6732b60050dbb94817291
Piyush Kumar <piyushkumar2111@outlook.com>
2026-08-30 11:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (5.73%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int idx=0;
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+                idx=i;
+            }
+        }
+        return idx;
+    }
+}
