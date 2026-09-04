a362ade8901ec3fe809fb842ed01e03e978e5f2b
Piyush Kumar <piyushkumar2111@outlook.com>
2026-09-04 23:38:06 +0530

    Time: 2 ms (100.00%) | Memory: 121.5 MB (68.47%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        // First find the min element in array
         int min=nums1[0];
         for(int x:nums1){
             if(x<min){
                 min=x;
             }
         }
+
+        // Check if the min element is odd....if odd , we can make all elements odd
         if(min%2!=0){
             return true;
         }else{
+
+            // if min is even, then all elements must ALREADY be even
             for(int x:nums1){
                 if(x%2!=0){
-                    return false;
+                    return false; // if an odd element is found then its impossible to convert
                 }
             }
         }
