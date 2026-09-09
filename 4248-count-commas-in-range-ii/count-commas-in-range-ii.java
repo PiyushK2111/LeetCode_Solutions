e33dec23f9d4486c069d518199280222649ccb8e
Piyush Kumar <piyushkumar2111@outlook.com>
2026-09-09 10:15:33 +0530

    Time: 1 ms (99.36%) | Memory: 42.4 MB (93.59%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n<1000){
+            return 0;
+        }
+        long total=0;
+        long start=1000;
+        while(start<=n){
+            total+=n-start+1;
+            start*=1000;
+        }
+        return total;
+    }
+}
